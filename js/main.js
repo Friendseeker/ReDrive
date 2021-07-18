0158531f4df7bbda549dc2a151a50d8e12889e82
Harshita Bhardwaj <honeyblue2604@gmail.com>
2021-07-17 17:44:09 -0700

    Choose country now Works!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 function showCities()
 {
     let country = document.getElementById("countrySelect").value.toLowerCase();
-    if(country != "choose")
+    if(country != "choose...")
     {
+        document.getElementById("countryPrompt").style.display = "none";
+        let locationArr = document.getElementsByClassName("location");
+        for(let l = 0; l < locationArr.length; l++)
+        {
+            console.log("in loop");
+            locationArr[l].style.display = "block";
+        }
+        //console.log(document.getElementsByClassName("location"));
 
-        console.log("change country");
+        console.log("changed country");
+    }
+    else if(country === "choose..."){
+        document.getElementById("countryPrompt").style.display = "block";
+        let locationArr = document.getElementsByClassName("location");
+        for(let l = 0; l < locationArr.length; l++)
+        {
+            locationArr[l].style.display = "none";
+        }
     }
     console.log(country);
 }
